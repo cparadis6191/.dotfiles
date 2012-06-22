6975a7d11d1e2860af3ccafae8c5ab1d9df93d8f
Chad Paradis <cparadis6191@gmail.com>
2012-06-21 20:25:54 -0400

    Added new config files to the linker script

--- link.sh
+++ link.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc bashrc tcshrc"    # list of files/folders to symlink in homedir
+files="vimrc bashrc tcshrc gitk gitconfig minttyrc"    # list of files/folders to symlink in homedir
 
 ##########
 
