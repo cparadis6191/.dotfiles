03e18656a10333446eeabb4c63fde7ee78b7090e
Chad Paradis <cparadis6191@gmail.com>
2013-11-19 23:32:14 -0500

    Changed the root directory to .dotfiles

--- link.sh
+++ link.sh
@@ -6,7 +6,7 @@
 
 ########## Variables ##########################################################
 
-dir=~/dotfiles					# dotfiles directory
+dir=~/.dotfiles					# dotfiles directory
 olddir=~/.dotfiles_old			# old dotfiles backup directory
 files="vimrc bashrc bash_profile gitconfig minttyrc"	# list of files/folders to symlink in homedir
 
