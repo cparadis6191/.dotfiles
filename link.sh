9da38e6a3dcf286fc053e21e529c23e11ca48067
Chad Paradis <cparadis6191@gmail.com>
2016-11-14 23:20:07 -0500

    Change from a bash script to GNU Stow for symlinking dotfiles

--- link.sh
+++ link.sh
@@ -1,41 +1,4 @@
 #!/usr/bin/env bash
-# Create symlinks in $HOME to the specified dotfiles in .dotfiles
 
-
-olddir="$HOME/.dotfiles.bak"    # Old dotfiles backup directory
-
-files="bash_profile bashrc gitconfig gitignore_global minttyrc vimrc"    # List of files/folders to symlink in homedir
-folders="vim config"
-files+=" $folders"
-
-# Create .dotfiles.bak in homedir
-if [ ! -d "$olddir" ]; then
-	echo "Creating $olddir for backup of any existing dotfiles in $HOME"
-	mkdir -p "$olddir"
-else
-	echo "$olddir already exists"
-fi
-
-# Create all necessary local folders
-for folder in $folders; do
-	if [ ! -d "$folder" ]; then
-		echo "Creating $folder to link to from $HOME"
-		mkdir -p "$folder"
-	fi
-done
-
-# Move any existing dotfiles in $HOME to $HOME/.dotfiles.bak, then create symlinks from $HOME to any files in the .dotfiles directory specified in $files
-for file in $files; do
-	if [ -h "$HOME/.$file" ]; then
-		echo "Symlink for .$file already exists"
-		continue
-	else
-		if [ -f "$HOME/.$file" ] || [ -d "$HOME/.$file" ]; then
-			echo "Moving $HOME/.$file to $olddir/.$file"
-			mv "$HOME/.$file" "$olddir/.$file"
-		fi
-	fi
-
-	echo "Making symlink from $HOME/.$file to $file"
-	ln -s "`pwd`/$file" "$HOME/.$file"
-done
+packages='bash git nvim vim'
+stow $packages
--- unlink.sh
+++ unlink.sh
@@ -1,38 +1,4 @@
 #!/usr/bin/env bash
-# Restore old dotfiles from $olddir back to $HOME
 
-
-olddir="$HOME/.dotfiles.bak"    # Old dotfiles backup directory
-
-files="bash_profile bashrc gitconfig gitignore_global minttyrc vimrc"    # List of files/folders to symlink in homedir
-folders="vim config"
-files+=" $folders"
-
-# Remove any symlinks to dotfiles and then move any dotfiles in $olddir back to the home directory, then remove the $olddir folder
-for file in $files; do
-	if [ -h "$HOME/.$file" ]; then
-		echo "Removing symlink for $HOME/.$file"
-		rm -f "$HOME/.$file"
-	else
-		echo "$HOME/.$file is not a symlink"
-	fi
-done
-
-if [ -d "$olddir" ]; then
-	echo "Old dotfiles detected. Starting restore procedure"
-
-	# Restore all files and folders from olddir
-	for oldfile in $olddir/* $olddir/.[^.]*; do
-		if [ -f "$oldfile" ] || [ -d "$oldfile" ]; then
-			echo "Restoring $oldfile to $HOME/`basename $oldfile`"
-			mv "$oldfile" "$HOME/`basename $oldfile`"
-		fi
-	done
-
-	echo "Removing $olddir"
-	rmdir "$olddir"
-
-	echo "Original dotfiles restored"
-else
-	echo "Old dotfiles not detected. Aborting restore procedure"
-fi
+packages='bash git nvim vim'
+stow --delete $packages
