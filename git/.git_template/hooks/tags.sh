568bbfb9b8da107c1e79dcae8b4b52b201af46e2
Chad Paradis <cparadis6191@gmail.com>
2017-07-24 23:30:00 -0400

    Rename tags to tags.h

--- git/.git_template/hooks/tags.sh
+++ git/.git_template/hooks/tags.sh
@@ -7,17 +7,21 @@ function remove_temps {
 	rm -f "$dir/$$.tags"
 
 	rm -f "$dir/$$.cscope.out"
+	rm -f "$dir/n$$.cscope.out"
 	rm -f "$dir/$$.cscope.out.in"
 	rm -f "$dir/$$.cscope.out.po"
 
 	rm -f "$dir/$$.files"
+	rm -f "$dir/$$.cscope.files"
 }
 trap 'remove_temps' EXIT
 
 git ls-files > "$dir/$$.files"
+awk '{ print "\""$0"\""}' "$dir/$$.files" > "$dir/$$.cscope.files"
+
 git submodule --quiet foreach 'git ls-files | sed "s|^|$path/|"' >> "$dir/$$.files"
 ctags --excmd=number --sort=foldcase --tag-relative -L "$dir/$$.files" -f "$dir/$$.tags"
-cscope -b -C -q -i "$dir/$$.files" -f "$dir/$$.cscope.out"
+cscope -b -C -q -i "$dir/$$.cscope.files" -f "$dir/$$.cscope.out"
 
 mv "$dir/$$.tags"          "$dir/tags"
 mv "$dir/$$.cscope.out"    "$dir/cscope.out"
