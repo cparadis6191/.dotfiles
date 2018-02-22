401fab1dc857661db6c751d3e250ffd881e112a5
Chad Paradis <cparadis6191@gmail.com>
2018-02-21 23:52:59 -0500

    Add a way to remove hooks
    Update some variable names in tags.sh.

--- git/.git_template/hooks/tags.sh
+++ git/.git_template/hooks/tags.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 set -e
-dir="$(git rev-parse --git-dir)"
+git_dir="$(git rev-parse --git-dir)"
 
-lsof -t "$dir/tags.lock" | xargs --no-run-if-empty kill -SIGINT
+lsof -t "$git_dir/tags.lock" | xargs --no-run-if-empty kill -SIGINT
 (
 flock --timeout 5 200
 
 function remove_temps {
-	rm -f "$dir/$$.tags"
+	rm -f "$git_dir/$$.tags"
 
-	rm -f "$dir/$$.cscope.out"
-	rm -f "$dir/n$$.cscope.out"
-	rm -f "$dir/$$.cscope.out.in"
-	rm -f "$dir/$$.cscope.out.po"
+	rm -f "$git_dir/$$.cscope.out"
+	rm -f "$git_dir/n$$.cscope.out"
+	rm -f "$git_dir/$$.cscope.out.in"
+	rm -f "$git_dir/$$.cscope.out.po"
 
-	rm -f "$dir/$$.files"
-	rm -f "$dir/$$.cscope.files"
+	rm -f "$git_dir/$$.files"
+	rm -f "$git_dir/$$.cscope.files"
 }
 trap 'remove_temps' EXIT
 
-git ls-files -z | tr "\0" "\n" > "$dir/$$.files"
-top="$(git rev-parse --show-toplevel)"
-awk --assign top="$top/" '{ print "\""top$0"\"" }' "$dir/$$.files" > "$dir/$$.cscope.files"
+git ls-files -z | tr "\0" "\n" > "$git_dir/$$.files"
+top_dir="$(git rev-parse --show-toplevel)"
+awk --assign top_dir="$top_dir/" '{ print "\""top_dir$0"\"" }' "$git_dir/$$.files" > "$git_dir/$$.cscope.files"
 
-ctags --excmd=number --sort=foldcase --tag-relative -L "$dir/$$.files" -f "$dir/$$.tags"
-cscope -b -C -q -i "$dir/$$.cscope.files" -f "$dir/$$.cscope.out"
+ctags --excmd=number --sort=foldcase --tag-relative -L "$git_dir/$$.files" -f "$git_dir/$$.tags"
+cscope -b -C -q -i "$git_dir/$$.cscope.files" -f "$git_dir/$$.cscope.out"
 
-mv "$dir/$$.tags"          "$dir/tags"
+mv "$git_dir/$$.tags"          "$git_dir/tags"
 
-mv "$dir/$$.cscope.out"    "$dir/cscope.out"
-mv "$dir/$$.cscope.out.in" "$dir/cscope.out.in"
-mv "$dir/$$.cscope.out.po" "$dir/cscope.out.po"
-) 200> "$dir/tags.lock"
+mv "$git_dir/$$.cscope.out"    "$git_dir/cscope.out"
+mv "$git_dir/$$.cscope.out.in" "$git_dir/cscope.out.in"
+mv "$git_dir/$$.cscope.out.po" "$git_dir/cscope.out.po"
+) 200> "$git_dir/tags.lock"
