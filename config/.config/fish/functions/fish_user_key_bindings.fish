8b54530aeb1bc6b214f659e2b43f81e740858475
Chad Paradis <cparadis6191@gmail.com>
2024-12-28 14:05:59 -0500

    Move fish `bind \t` impl into a function

--- config/.config/fish/functions/fish_user_key_bindings.fish
+++ config/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,12 @@
+function fish_tab_complete_more_like_bash
+    if not commandline --paging-mode
+        commandline --function complete
+    end
+end
+
 function fish_user_key_bindings
     bind --erase --preset \cd
+
+    # Make tab complete more like bash.
+    bind \t fish_tab_complete_more_like_bash
 end
