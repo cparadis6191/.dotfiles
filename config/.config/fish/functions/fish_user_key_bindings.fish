a96a14e80d4a27eeb26f2638385ffc5f21993d45
Chad Paradis <cparadis6191@gmail.com>
2024-12-28 14:29:14 -0500

    Approximate bash `set -o ignoreeof` in fish

--- config/.config/fish/functions/fish_user_key_bindings.fish
+++ config/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,12 @@
+function fish_use_exit_to_leave_the_shell
+    if test -z (commandline --current-buffer)
+        echo '
+Use "exit" to leave the shell.'
+
+        commandline --function repaint
+    end
+end
+
 function fish_tab_complete_more_like_bash
     if not commandline --paging-mode
         commandline --function complete
@@ -5,7 +14,8 @@ function fish_tab_complete_more_like_bash
 end
 
 function fish_user_key_bindings
-    bind --erase --preset \cd
+    # Approximate bash `set -o ignoreeof`.
+    bind \cd fish_use_exit_to_leave_the_shell
 
     # Make tab complete more like bash.
     bind \t fish_tab_complete_more_like_bash
