4f2c93274fee9159a71cdb9a99af71f57fa6b924
Chad Paradis <cparadis6191@gmail.com>
2025-01-19 21:32:24 -0500

    Link most configuration files in home.nix

--- home.nix
+++ home.nix
@@ -42,7 +42,64 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".alacritty.toml".source = ./alacritty/.alacritty.toml;
+    ".alacritty.yml".source = ./alacritty/.alacritty.yml;
 
+    ".bash_profile".source = ./bash/.bash_profile;
+    ".bashrc".source = ./bash/.bashrc;
+
+    ".config/bat/config".source = ./config/.config/bat/config;
+
+    ".config/fish/.gitignore".source = ./config/.config/fish/.gitignore;
+    ".config/fish/config.fish".source = ./config/.config/fish/config.fish;
+    ".config/fish/functions/fish_prompt.fish".source = ./config/.config/fish/functions/fish_prompt.fish;
+    ".config/fish/functions/fish_title.fish".source = ./config/.config/fish/functions/fish_title.fish;
+    ".config/fish/functions/fish_user_key_bindings.fish".source = ./config/.config/fish/functions/fish_user_key_bindings.fish;
+
+    ".git_template/hooks/post-checkout".source = ./git/.git_template/hooks/post-checkout;
+    ".git_template/hooks/post-commit".source = ./git/.git_template/hooks/post-commit;
+    ".git_template/hooks/post-merge".source = ./git/.git_template/hooks/post-merge;
+    ".git_template/hooks/post-rewrite".source = ./git/.git_template/hooks/post-rewrite;
+    ".git_template/hooks/tags".source = ./git/.git_template/hooks/tags;
+    ".gitconfig".source = ./git/.gitconfig;
+    ".gitignore_global".source = ./git/.gitignore_global;
+
+    ".inputrc".source = ./inputrc/.inputrc;
+
+    ".tmux.conf".source = ./tmux/.tmux.conf;
+
+    ".vim/plugin/fzf_neosnippets/plugin/fzf_neosnippets.vim".source = ./vim/.vim/plugin/fzf_neosnippets/plugin/fzf_neosnippets.vim;
+    ".vim/plugin/fzf_quickfix/plugin/fzf_quickfix.vim".source = ./vim/.vim/plugin/fzf_quickfix/plugin/fzf_quickfix.vim;
+    ".vim/plugin/journal/ftdetect/journal.vim".source = ./vim/.vim/plugin/journal/ftdetect/journal.vim;
+    ".vim/plugin/journal/neosnippets/journal.snip".source = ./vim/.vim/plugin/journal/neosnippets/journal.snip;
+    ".vim/plugin/journal/syntax/journal.vim".source = ./vim/.vim/plugin/journal/syntax/journal.vim;
+    ".vim/plugin/normal_expr/plugin/normal_expr.vim".source = ./vim/.vim/plugin/normal_expr/plugin/normal_expr.vim;
+    ".vim/plugin/note/autoload/note.vim".source = ./vim/.vim/plugin/note/autoload/note.vim;
+    ".vim/plugin/note/plugin/note.vim".source = ./vim/.vim/plugin/note/plugin/note.vim;
+    ".vim/plugin/search/plugin/search.vim".source = ./vim/.vim/plugin/search/plugin/search.vim;
+    ".vim/plugin/visual_selection/autoload/visual_selection.vim".source = ./vim/.vim/plugin/visual_selection/autoload/visual_selection.vim;
+    ".vimrc".source = ./vim/.vimrc;
+
+    ".local/bin/binxxd".source = ./local/.local/bin/binxxd;
+    ".local/bin/calc".source = ./local/.local/bin/calc;
+    ".local/bin/cdb".source = ./local/.local/bin/cdb-impl;
+    ".local/bin/cxxd".source = ./local/.local/bin/cxxd;
+    ".local/bin/hexxd".source = ./local/.local/bin/hexxd;
+    ".local/bin/isotounix".source = ./local/.local/bin/isotounix;
+    ".local/bin/journal".source = ./local/.local/bin/journal;
+    ".local/bin/journals".source = ./local/.local/bin/journals;
+    ".local/bin/la".source = ./local/.local/bin/la;
+    ".local/bin/ll".source = ./local/.local/bin/ll;
+    ".local/bin/mkb".source = ./local/.local/bin/mkb;
+    ".local/bin/post".source = ./local/.local/bin/post;
+    ".local/bin/pre".source = ./local/.local/bin/pre;
+    ".local/bin/repeat".source = ./local/.local/bin/repeat;
+    ".local/bin/stopwatch".source = ./local/.local/bin/stopwatch;
+    ".local/bin/texxd".source = ./local/.local/bin/texxd;
+    ".local/bin/unixtoiso".source = ./local/.local/bin/unixtoiso;
+    ".local/bin/unixtolocaliso".source = ./local/.local/bin/unixtolocaliso;
+    ".local/bin/vim".source = ./local/.local/bin/vim;
+    ".local/bin/vrep".source = ./local/.local/bin/vrep;
   };
 
   # Home Manager can also manage your environment variables through
