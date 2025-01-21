70f5a1c15e83a89e9464caf83dc3b36349b432a0
Chad Paradis <cparadis6191@gmail.com>
2025-01-20 20:05:49 -0500

    Let Nix manage flakes in bootstrap-home-manager.sh

--- bootstrap-home-manager.sh
+++ bootstrap-home-manager.sh
@@ -2,6 +2,8 @@
 
 # See https://nix-community.github.io/home-manager/#sec-install-standalone for
 # more information.
+echo 'experimental-features = nix-command flakes' > "$HOME/.config/nix/nix.conf"
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
