41ac931521a9101ae14ee5ae35b7800354017212
Chad Paradis <cparadis6191@gmail.com>
2025-01-21 21:30:20 -0500

    Source nix.sh in bootstrap-home-manager.sh

--- bootstrap-home-manager.sh
+++ bootstrap-home-manager.sh
@@ -7,6 +7,8 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 # more information.
 echo 'experimental-features = nix-command flakes' > "$HOME/.config/nix/nix.conf"
 
+source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
