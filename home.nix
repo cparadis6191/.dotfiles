50b3aa10877439723f489b3ee840fa9f4e8a4030
Chad Paradis <cparadis6191@gmail.com>
2025-01-26 10:53:21 -0500

    Move home.activation to activation.nix

--- home.nix
+++ home.nix
@@ -16,6 +16,7 @@
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
   imports = [
+    ./activation.nix
     ./file.nix
     ./packages.nix
   ];
@@ -42,45 +43,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  home.activation = {
-    makeLocalEtc = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -d "$HOME/.local/etc" ]; then
-        run mkdir --parents "$HOME/.local/etc"
-      fi
-    '';
-
-    tmuxAttachDuringBashLogin = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -e "$HOME/.local/etc/.tmux_attach_during_bash_login" ]; then
-      	# Quoting or escaping the "limit string" at the head of a here document
-      	# disables parameter substitution within its body.
-      	run cat << 'HEREDOC' >> "$HOME/.local/etc/.bash_profile"
-      # Attach to an existing tmux session if no client is attached, otherwise start
-      # a new session.
-      if [[ $TMUX == ''' && $(command -v 'tmux') != ''' ]]; then
-      	delim=$'\t'
-      	tmux_detached_session="$(tmux list-sessions -F "#{session_id}$delim#{session_attached}" 2> /dev/null |
-      		grep --basic-regexp --regexp='0$' |
-      		head --lines=1 |
-      		cut --fields=1)"
-      	if [[ $tmux_detached_session != ''' ]]; then
-      		exec tmux attach-session -t "$tmux_detached_session" \; unbind-key d
-      	else
-      		exec tmux new-session \; unbind-key d
-      	fi
-      fi
-      HEREDOC
-
-      	run touch "$HOME/.local/etc/.tmux_attach_during_bash_login"
-      fi
-    '';
-
-    createAndActivateVirtualenv = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -d "$HOME/.virtualenv" ]; then
-        run ${pkgs.virtualenv}/bin/virtualenv --download "$HOME/.virtualenv"
-        run echo 'VIRTUAL_ENV_DISABLE_PROMPT=1' >> "$HOME/.local/etc/.bash_profile"
-        run echo 'source "$HOME/.virtualenv/bin/activate"' >> "$HOME/.local/etc/.bash_profile"
-      fi
-    '';
-  };
 }
