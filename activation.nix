4f16c8816369db6319181f288da0d8d6de3de653
Chad Paradis <cparadis6191@gmail.com>
2025-08-27 22:59:46 -0400

    Move things from local .bashrc to local .bash_profile in activation.nix

--- activation.nix
+++ activation.nix
@@ -7,8 +7,8 @@
       	run mkdir --parents "$HOME/.local/bin"
       fi
 
-      if [ "$(grep -c 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.local/etc/.bashrc")" -eq 0 ]; then
-      	run echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.local/etc/.bashrc"
+      if [ "$(grep -c 'export PATH="$HOME/.local/bin:$PATH"' "$HOME/.local/etc/.bash_profile")" -eq 0 ]; then
+      	run echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.local/etc/.bash_profile"
       fi
     '';
 
@@ -133,12 +133,12 @@
       	run ${pkgs.virtualenv}/bin/virtualenv --download "$HOME/.virtualenv"
       fi
 
-      if [ "$(grep -c 'VIRTUAL_ENV_DISABLE_PROMPT=1' "$HOME/.local/etc/.bashrc")" -eq 0 ]; then
-      	run echo 'VIRTUAL_ENV_DISABLE_PROMPT=1' >> "$HOME/.local/etc/.bashrc"
+      if [ "$(grep -c 'VIRTUAL_ENV_DISABLE_PROMPT=1' "$HOME/.local/etc/.bash_profile")" -eq 0 ]; then
+      	run echo 'VIRTUAL_ENV_DISABLE_PROMPT=1' >> "$HOME/.local/etc/.bash_profile"
       fi
 
-      if [ "$(grep -c 'source "$HOME/.virtualenv/bin/activate"' "$HOME/.local/etc/.bashrc")" -eq 0 ]; then
-      	run echo 'source "$HOME/.virtualenv/bin/activate"' >> "$HOME/.local/etc/.bashrc"
+      if [ "$(grep -c 'source "$HOME/.virtualenv/bin/activate"' "$HOME/.local/etc/.bash_profile")" -eq 0 ]; then
+      	run echo 'source "$HOME/.virtualenv/bin/activate"' >> "$HOME/.local/etc/.bash_profile"
       fi
     '';
   };
