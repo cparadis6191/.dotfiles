dd6c8045e1d5607ea5abb30468f85b6903d735aa
Chad Paradis <cparadis6191@gmail.com>
2026-02-22 09:00:50 -0500

    Source nix.sh in local .bash_profile earlier in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,8 @@ sed --expression='/# added by Nix installer/d' --in-place "$HOME/.config/fish/co
 mkdir --parents "$HOME/.config/nix"
 echo 'experimental-features = nix-command flakes' >> "$HOME/.config/nix/nix.conf"
 
+echo 'source "$HOME/.nix-profile/etc/profile.d/nix.sh"' >> "$HOME/.local/etc/.bash_profile"
+
 source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/1cfa305fba94468f665de1bd1b62dddf2e0cb012.tar.gz home-manager
@@ -19,5 +21,3 @@ nix-channel --update
 nix-shell '<home-manager>' --attr install
 
 home-manager switch -b bak --flake "$(realpath "$(dirname "${BASH_SOURCE[0]}")")#chad"
-
-echo 'source "$HOME/.nix-profile/etc/profile.d/nix.sh"' >> "$HOME/.local/etc/.bash_profile"
