7e0ed436ad22bd0af28502c89cf35ea03171fbc9
Chad Paradis <cparadis6191@gmail.com>
2026-04-26 20:36:01 -0400

    Force WezTerm cursor to use reverse video colors

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -6,6 +6,10 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 
+-- Force the cursor to use reverse video colors based on the `foreground` and
+-- `background` colors.
+config.force_reverse_video_cursor = true
+
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
